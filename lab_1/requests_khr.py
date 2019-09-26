2aca466f1c6d6e29edcf5e84ffdb6e3d91ec12b1
SvKh99 <kris.alex.sveta@gmail.com>
2019-09-26 15:33:55 +0300

    main func divided

--- lab_1/requests_khr.py
+++ lab_1/requests_khr.py
@@ -4,43 +4,52 @@ import json
 import datetime
 from flask import Flask
 from flask import render_template
-import unittest
+
+
+def parse_page(url):
+    yandex_politics_request = requests.get(url)
+
+    if yandex_politics_request.status_code == 200:
+        print('Yay! We performed a successfull request')
+    else:
+        print('Oops! Something went wrong...')
+
+    return BeautifulSoup(yandex_politics_request.text, 'html.parser')
+
+
+def get_tittles(parsed_page):
+    articles = []
+    for title_tag in parsed_page.find_all('h2'):
+        articles.append({"tittle": title_tag.text})
+
+    return articles
+
+
+def record_json(url, today, articles):
+    json_articles = json.dumps({
+        "url": url,
+        "creationDate": today,
+        "articles": articles
+    }, ensure_ascii=False)
+
+    with open("articles.json", "w", encoding="utf-8") as file:
+        file.write(json_articles)
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-class TestStringMethods(unittest.TestCase):
+def get_news():
+    url = "https://yandex.com/news/rubric/politics?from=index"
+    today = str(datetime.date.today())
 
-    def get_news(self):
-        url = "https://yandex.com/news/rubric/politics?from=index"
-        today = str(datetime.date.today())
+    page = parse_page(url)
+    tittles = get_tittles(page)
+    record_json(url, today, tittles)
 
-        yandex_politics_request = requests.get(url)
-
-        if yandex_politics_request.status_code == 200:
-            print('Yay! We performed a successfull request')
-        else:
-            print('Oops! Something went wrong...')
-
-        parsed_page = BeautifulSoup(yandex_politics_request.text, 'html.parser')
-
-        articles = []
-        for title_tag in parsed_page.find_all('h2'):
-            articles.append({"tittle": title_tag.text})
-
-        json_articles = json.dumps({
-            "url": "https://yandex.com/news/rubric/politics?from=index",
-            "creationDate": today,
-            "articles": articles
-        }, ensure_ascii=False)
-
-        with open("articles.json", "w", encoding="utf-8") as file:
-            file.write(json_articles)
-
-        return render_template('news_page.html', url=url, date=today, articles=articles)
+    return render_template('news_page.html', url=url, date=today, articles=tittles)
 
 
 if __name__ == '__main__':
     app.run()
-    unittest.main()
