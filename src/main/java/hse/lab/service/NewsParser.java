1db4ce49b339de360194861b055b35424501d332
APalashinovich <anna.palashinovich@firstlinesoftware.com>
2019-09-29 22:56:18 +0300

    init architecture commit

---
+++ src/main/java/hse/lab/service/NewsParser.java
@@ -0,0 +1,16 @@
+package hse.lab.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NewsParser {
+    public void parseNews() {
+
+    }
+
+    public List<String> getNews() {
+        return null;
+    }
+}
