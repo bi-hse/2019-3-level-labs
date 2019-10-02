3291603b46c133feed6379483ee8a4b7a7354748
anitapalash <647316392@github.com>
2019-10-02 12:09:39 +0300

    added tests, getHtmlPage method

--- src/main/java/hse/lab/controller/NewsController.java
+++ src/main/java/hse/lab/controller/NewsController.java
@@ -1,12 +1,23 @@
 package hse.lab.controller;
 
-import org.springframework.stereotype.Controller;
+import hse.lab.service.NewsParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-class NewsController {
-    @GetMapping
-    public void getNews() {
+@RestController
+public class NewsController {
 
+    @Autowired
+    private NewsParser newsParser;
+
+    @GetMapping("/news")
+    public String getNews() {
+        return null;
+    }
+
+    @GetMapping("/pageTitle")
+    public String getTitle() {
+        return newsParser.getHtmlPage().title();
     }
 }
