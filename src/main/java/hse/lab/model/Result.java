eb232b93294c45e100bbff664339619a3506eaf3
anitapalash <647316392@github.com>
2019-10-05 15:35:48 +0300

    modified result class

--- src/main/java/hse/lab/model/Result.java
+++ src/main/java/hse/lab/model/Result.java
@@ -7,4 +7,34 @@ public class Result {
     private String url;
     private Date creationDate;
     private List<String> articles;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<String> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<String> articles) {
+        this.articles = articles;
+    }
+
+    public Result(String url, Date creationDate, List<String> articles) {
+        this.url = url;
+        this.creationDate = creationDate;
+        this.articles = articles;
+    }
 }
