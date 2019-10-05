0da63a7bcdaf21dc7feb76821fb37e95f2d50550
tomuvon <vatlirina@gmail.com>
2019-10-05 15:06:32 +0300

    Added result class

---
+++ src/main/java/hse/lab/model/Result.java
@@ -0,0 +1,10 @@
+package hse.lab.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Result {
+    private String url;
+    private Date creationDate;
+    private List<String> articles;
+}
