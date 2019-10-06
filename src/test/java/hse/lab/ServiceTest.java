2d20924be1c64dbe957f2aea79f00057dcdf2bec
tomuvon <vatlirina@gmail.com>
2019-10-06 11:58:13 +0300

    Added headers test

--- src/test/java/hse/lab/ServiceTest.java
+++ src/test/java/hse/lab/ServiceTest.java
@@ -6,6 +6,7 @@ import hse.lab.service.NewsParser;
 import org.json.JSONException;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -39,5 +42,15 @@ public class ServiceTest {
         Connection.Response response = Jsoup.connect(TPROGER_URL).followRedirects(false).execute();
         System.out.println(response.statusCode());
         Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    public void headersTest() throws IOException {
+        File savedPage = new File("src/test/resources/static/tproger.html");
+        Document doc = Jsoup.parse(savedPage, "UTF-8", TPROGER_URL);
+        List<String> articles = parser.findArticles(doc);
+        Assert.assertNotNull(articles);
+        System.out.println(articles.size());
+        Assert.assertEquals(articles.size(), 36);
     }
 }
