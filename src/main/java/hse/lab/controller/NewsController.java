828c72535557b167ddc93a65720231a90a554798
tomuvon <vatlirina@gmail.com>
2019-10-06 13:50:08 +0300

    Added new dependencies and new versions of NewsController and PageFormer

--- src/main/java/hse/lab/controller/NewsController.java
+++ src/main/java/hse/lab/controller/NewsController.java
@@ -3,10 +3,12 @@ package hse.lab.controller;
 import hse.lab.service.NewsParser;
 import hse.lab.service.PageFormer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class NewsController {
 
     private final String TPROGER_URL = "https://tproger.ru/";
@@ -17,12 +19,12 @@ public class NewsController {
     @Autowired
     private PageFormer former;
 
-    @GetMapping("/news")
-    public void getNews() {
-        former.createPage(newsParser.findArticles(newsParser.getHtmlPage(TPROGER_URL)));
+    @RequestMapping(value = "/news", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String getNews() {
+        return former.createPage(newsParser.findArticles(newsParser.getHtmlPage(TPROGER_URL)));
     }
 
-    @GetMapping("/pageTitle")
+    @RequestMapping(value = "/pageTitle", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String getTitle() {
         return newsParser.getHtmlPage(TPROGER_URL).title();
     }
