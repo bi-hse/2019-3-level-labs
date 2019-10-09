49fd117f6c8f55f5a2b8cde1f22c3a03702f07b0
Raushaniia <muradimovarr@yandex.ru>
2019-10-09 18:14:49 +0300

    update test_html

--- lab_1/tests/test_html.py
+++ lab_1/tests/test_html.py
@@ -1,4 +1,4 @@
-from lab_1.news import app, get_response, find_articles
+from lab_1.news import app, get_response
 import unittest
 import datetime
 from bs4 import BeautifulSoup
@@ -12,6 +12,12 @@ class HtmlTest(unittest.TestCase):
         request = get_response("https://yandex.com/news/rubric/politics?from=index")
         self.assertEqual(request.status_code, 200)
         response = self.app.get('/', follow_redirects=True)
+
+        def find_articles(html_page):
+            articles = []
+            for title_tag in html_page.find_all('li'):
+                articles.append({"tittle": title_tag.text})
+            return articles
         articles = find_articles(BeautifulSoup(response.data, 'html.parser'))
         self.assertNotEqual("", ['articles'][0]['tittle'])
     if __name__ == "__main__":
