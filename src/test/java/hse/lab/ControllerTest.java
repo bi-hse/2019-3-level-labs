2c9288299892b5d06dbae6bc6ef4e0749a2aa003
anitapalash <anitapalash98@gmail.com>
2019-10-09 10:22:20 +0300

    test fix

--- src/test/java/hse/lab/ControllerTest.java
+++ src/test/java/hse/lab/ControllerTest.java
@@ -1,14 +1,18 @@
 package hse.lab;
 
+import hse.lab.service.NewsParser;
+import hse.lab.service.PageFormer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -16,11 +20,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class ControllerTest {
 
+    private final String TPROGER_URL = "https://tproger.ru/";
+
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private NewsParser parser;
+
+    @Autowired
+    private PageFormer former;
+
     @Test
     public void urlIsAccessible() throws Exception {
-        this.mvc.perform(get("/news")).andExpect(status().isOk());
+        former.createPage(parser.findArticles(parser.getHtmlPage(TPROGER_URL)));
+        this.mvc.perform(get("/news"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
