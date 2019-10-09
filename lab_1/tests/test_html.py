5bd8835cbbd713c92ed38713dfd9574cf8b9af4b
Raushaniia <muradimovarr@yandex.ru>
2019-10-09 19:03:59 +0300

    Update html_page

--- lab_1/tests/test_html.py
+++ lab_1/tests/test_html.py
@@ -12,13 +12,10 @@ class HtmlTest(unittest.TestCase):
         request = get_response("https://yandex.com/news/rubric/politics?from=index")
         self.assertEqual(request.status_code, 200)
         response = self.app.get('/', follow_redirects=True)
-
-        def find_articles(html_page):
-            articles = []
-            for title_tag in html_page.find_all('li'):
-                articles.append({"tittle": title_tag.text})
-            return articles
-        articles = find_articles(BeautifulSoup(response.data, 'html.parser'))
-        self.assertNotEqual("", ['articles'][0]['tittle'])
+        html_page=(BeautifulSoup(response.data, 'html.parser'))
+        articles = []
+        for title_tag in html_page.find_all('li'):
+            articles.append({"tittle": title_tag.text})
+        self.assertNotEqual(articles, ['articles'][0]['tittle'])
     if __name__ == "__main__":
         unittest.main()
